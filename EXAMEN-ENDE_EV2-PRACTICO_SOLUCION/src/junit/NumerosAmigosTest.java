52d7ee8e245e80231ef8208772af5102dc38c74e
Alejandro <alejandromillanmolinero@gmail.com>
2023-02-20 11:16:07 +0100

    Validación inicial

---
+++ EXAMEN-ENDE_EV2-PRACTICO_SOLUCION/src/junit/NumerosAmigosTest.java
@@ -0,0 +1,31 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumerosAmigosTest {
+	
+	@Test
+	public void testAmigoMal() {
+		boolean esAmigo = false;
+		boolean resultado=amigo();
+		
+	}
+
+	@Test
+	public void testNumerosAmigos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAmigo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEsN1IgualAN2() {
+		fail("Not yet implemented");
+	}
+
+}
